c23faefcb42920dd0b1a2a1a93778719052e95f2
Hownameee <namhaohuynh@gmail.com>
2026-01-06 11:26:00 +0700

    fix dev remove duplicate .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
+export default defineConfig(() => {
   return {
     root: 'client',
     plugins: [react(), tailwindcss()],
     server: {
       proxy: {
-        '/api': `http://localhost:${env.PORT}`,
+        '/api': `http://localhost:${process.env.PORT}`,
       },
     },
     build: {
